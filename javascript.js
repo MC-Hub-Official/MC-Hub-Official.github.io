72a9961171a742bcb467db22e698f801aba77917
Aman <63245705+thegeek101@users.noreply.github.com>
2020-09-13 13:49:13 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 function setupMenu() {
-    $("#nav").html() = '<div class="logo"><img src="MC-Hub.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Pages<i class="fa fa-caret-down"></i></button><div class="dropdown-content"><a href="index.html">Hub</a><a href="tips.html">Tips</a><a href="facts.html">Facts</a><a href="seeds.html">Seeds</a><a href="commands.html">Commands</a></div></div></div><br><br><br>';
+    $("#nav").html('<div class="logo"><img src="MC-Hub.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Pages<i class="fa fa-caret-down"></i></button><div class="dropdown-content"><a href="index.html">Hub</a><a href="tips.html">Tips</a><a href="facts.html">Facts</a><a href="seeds.html">Seeds</a><a href="commands.html">Commands</a></div></div></div><br><br><br>');
 }
