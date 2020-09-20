a00a4ace291a5c6a6cd02d7bf506a2428bdb71de
Blaaze <70574345+HBBX11@users.noreply.github.com>
2020-09-20 17:51:31 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,7 @@ function setupMenu() {
     $("#nav").html('<div class="logo"><img src="MC-Hub.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Menu<i class="fa fa-caret-down"></i></button><div class="dropdown-content"><a href="index.html">Hub</a><a href="tips.html">Tips</a><a href="facts.html">Facts</a><a href="seeds.html">Seeds</a><a href="commands.html">Commands</a></div></div></div><br><br><br>');
     $("head").html('<script async src="https://cse.google.com/cse.js?cx=c9e60bf9b9fec089e"></script><link rel="stylesheet" href="style.css"><link rel="stylesheet" href="navbar.css"><link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"><title> MC Hub </title>');
 }
+function specialSetup() {
+    $("#nav").html('<div class="logo"><img src="Screenshot 2020-09-20 at 08.20.11.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Menu<i class="fa fa-caret-down"></i></button><div class="dropdown-content"><a href="index.html">Hub</a><a href="tips.html">Tips</a><a href="facts.html">Facts</a><a href="seeds.html">Seeds</a><a href="commands.html">Commands</a></div></div></div><br><br><br>');
+    $("head").html('<script async src="https://cse.google.com/cse.js?cx=c9e60bf9b9fec089e"></script><link rel="stylesheet" href="style.css"><link rel="stylesheet" href="navbar.css"><link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"><title> MC Hub </title>');
+}
