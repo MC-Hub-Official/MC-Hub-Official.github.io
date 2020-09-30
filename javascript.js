4f2b32106fa2e3cd4b008b3c1d95de8695e43a2e
Aman <63245705+thegeek101@users.noreply.github.com>
2020-09-30 18:58:35 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,5 +2,5 @@
 function setupMenu() {
     $("#nav").html('<div class="logo"><img src="MC-Hub.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Menu</button><div class="dropdown-content"><a href="index.html">Hub</a><a href="tips.html">Tips</a><a href="facts.html">Facts</a><a href="seeds.html">Seeds</a><a href="commands.html">Commands</a></div></div></div><br><br><br>');
     $("head").html('<script async src="https://cse.google.com/cse.js?cx=c9e60bf9b9fec089e"></script><link rel="stylesheet" href="style.css"><link rel="stylesheet" href="navbar.css"><link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"><title> MC Hub </title>');
-    $("#foot").html('<a href=https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg">Youtube</a><a href="https://account.xbox.com/en-gb/profile?gamertag=MCHubSite">Xbox</a>');
+    $("#foot").html('<a href=https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg" style="float:left; color: #FF0000; font-size: 28px;">Youtube</a><a href="https://account.xbox.com/en-gb/profile?gamertag=MCHubSite" style="float:right; color: #4B8A08; font-size: 28px;">Xbox</a>');
 }
