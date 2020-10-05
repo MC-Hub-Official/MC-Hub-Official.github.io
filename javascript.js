a9e1c252f486905d5de6a2cc2b935ca98bffa317
Blaaze <70574345+HBBX11@users.noreply.github.com>
2020-10-05 18:28:30 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,3 +4,7 @@ function setupMenu() {
     $("head").html('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179487034-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-179487034-1");</script><script async src="https://cse.google.com/cse.js?cx=c9e60bf9b9fec089e"></script><link rel="stylesheet" href="style.css"><link rel="stylesheet" href="navbar.css"><link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"><title> MC Hub </title>');
     $("#foot").html('<a href="https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg" style="float:right; color: #FF0000; font-size: 28px;">Youtube</a><a href="https://account.xbox.com/en-gb/profile?gamertag=MCHubSite" style="float:left; color: #4B8A08; font-size: 28px;">Xbox</a>');
 }
+
+class hi {
+    public static void main(String[] args)
+    System.out.printIn('hi')
