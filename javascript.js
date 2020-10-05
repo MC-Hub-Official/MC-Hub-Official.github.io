0e07559cd7bb4c843afd3d295d249ae7e8f4d520
Blaaze <70574345+HBBX11@users.noreply.github.com>
2020-10-05 18:29:18 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,6 +5,3 @@ function setupMenu() {
     $("#foot").html('<a href="https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg" style="float:right; color: #FF0000; font-size: 28px;">Youtube</a><a href="https://account.xbox.com/en-gb/profile?gamertag=MCHubSite" style="float:left; color: #4B8A08; font-size: 28px;">Xbox</a>');
 }
 
-class hi {
-    public static void main(String[] args)
-    System.out.printIn('hi')
