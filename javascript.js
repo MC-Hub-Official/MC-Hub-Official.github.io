2f638d991817326b95ca82d2213075de98908250
Benliam12 <s100live.ca@live.ca>
2020-10-06 02:56:34 -0400

    Cleaning some of the code
    I felt bad.

--- javascript.js
+++
@@ -1,7 +0,0 @@
-// DO NOT EDIT THIS: VITAL FILE
-function setupMenu() {
-    $("#nav").html('<div class="logo"><img src="MC-Hub.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Menu</button><div class="dropdown-content"><a href="index.html">Hub</a><a href="tips.html">Tips</a><a href="facts.html">Facts</a><a href="seeds.html">Seeds</a><a href="commands.html">Commands</a></div></div></div><br><br><br>');
-    $("head").html('<script src="https://www.googleoptimize.com/optimize.js?id=OPT-TNV37V3"></script><script async src="https://www.googletagmanager.com/gtag/js?id=UA-179487034-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-179487034-1");</script><script async src="https://cse.google.com/cse.js?cx=c9e60bf9b9fec089e"></script><link rel="stylesheet" href="style.css"><link rel="stylesheet" href="navbar.css"><link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet"><title> MC Hub </title>');
-    $("#foot").html('<a href="https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg" style="float:right; color: #FF0000; font-size: 28px;">Youtube</a><a href="https://account.xbox.com/en-gb/profile?gamertag=MCHubSite" style="float:left; color: #4B8A08; font-size: 28px;">Xbox</a>');
-}
-
