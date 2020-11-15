ab216336d95a1132a81c1734a0bf3f1f1ff6b27e
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2020-11-15 13:44:43 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ var bgImages = ["url(https://cdn.mchub.ga/backgrounds/0.png)", "url(https://cdn.
 
 class MCHubHeader extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = '<div class="logo"><img src="https://cdn.mchub.ga/logos/logo.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Menu</button><div class="dropdown-content"><a href="/">Hub</a><a href="/tips">Tips</a><a href="/facts">Facts</a><a href="/seeds">Seeds</a><a href="/commands">Commands</a><a href="/search"> Search 🔎 </a></div></div></div><br><br><br>';
+    this.innerHTML = '<div class="logo"><img src="https://cdn.mchub.ga/logos/logo.png" width="260" height="63"/><br></div><div class="navbar"><div class="dropdown"><button class="dropbtn">Menu</button><div class="dropdown-content"><a href="/">Hub</a><a href="/tips">Tips</a><a href="/facts">Facts</a><a href="/seeds">Seeds</a><a href="/commands">Commands</a><a href="/search"> Search </a></div></div></div><br><br><br>';
   }
 }
 
