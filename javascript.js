0e54c998eb6c34cf7f99b0ae78406ba2e84ebd6a
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2020-11-23 17:39:21 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ class MCHubHeader extends HTMLElement {
 class MCHubFooter extends HTMLElement {
   connectedCallback() {
     // Insert social share buttons
-    this.insertAdjacentHTML("beforeend", '<a href="https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg?sub_confirmation=1" class="fa fa-youtube"></a><a href="/rss.xml" class="fa fa-rss" download></a>');
+    this.insertAdjacentHTML("beforeend", '<a href="https://www.youtube.com/channel/UCP2347E62P5fcoC2eK8QiPg?sub_confirmation=1"></a><a href="/rss.xml" class="fa fa-rss" download></a>');
     
     // Insert RSS link
     this.insertAdjacentHTML("beforeend", '<br><div style="font-size: 24; text-align: center; display: inline;"><a class="orange" style="display: inline;" href="/rss.xml" download> Subscribe to RSS for MC Hub! </a></div><br>');
@@ -37,8 +37,8 @@ function setup() {
   head.insertAdjacentHTML("beforeend", '<title> MC Hub </title><meta name="description" content="MC Hub is a free website with amazing tips, seeds, facts and command guides to help you become a Minecraft PRO in no time!"><link rel="icon" href="https://exf.mchub.ga/logos/favicon.ico" type="image/x-icon"><meta name="viewport" content="width=device-width, initial-scale=1">');
   
   // Load (and run) external scripts
-  head.insertAdjacentHTML("beforeend", '<script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5fba9394feada30012a6b8a0&product=inline-share-buttons" async="async"></script><script src="https://www.googleoptimize.com/optimize.js?id=OPT-TNV37V3"></script><script async src="https://www.googletagmanager.com/gtag/js?id=UA-179487034-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-179487034-1");</script>');
+  head.insertAdjacentHTML("beforeend", '<script src="https://kit.fontawesome.com/1e53eeabd1.js" crossorigin="anonymous"></script><script src="https://www.googleoptimize.com/optimize.js?id=OPT-TNV37V3"></script><script async src="https://www.googletagmanager.com/gtag/js?id=UA-179487034-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-179487034-1");</script>');
   
   // Import CSS and other style files
-  head.insertAdjacentHTML("beforeend", '<link rel="stylesheet" href="/css/social.css"><link rel="stylesheet" href="/css/style.css"><link rel="stylesheet" href="/css/navbar.css"><link rel="stylesheet" href="/css/colours.css"><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
+  head.insertAdjacentHTML("beforeend", '<link rel="stylesheet" href="/css/social.css"><link rel="stylesheet" href="/css/style.css"><link rel="stylesheet" href="/css/navbar.css"><link rel="stylesheet" href="/css/colours.css"><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap">');
 }
