5a77d54cbab6d0d893c2f74da0a5166fb86d841d
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2021-01-27 10:13:12 +0000

    Delete pwa.js

--- pwa.js
+++
@@ -1,5 +0,0 @@
-document.head.insertAdjacentHTML("beforeend", '<meta name="theme-color" content="#0174DF"/><link rel="apple-touch-icon" href="https://exf.minecraft-hub.com/logos/icon-192.png"><link rel="manifest" href="/manifest.webmanifest">');
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.prompt();
-}
