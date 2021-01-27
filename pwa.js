cebcfaccb24c81eb5d43f585a648bced33aeba09
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2021-01-27 10:04:25 +0000

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -1,6 +1,6 @@
 let deferredPrompt;
 
-head.insertAdjacentHTML("beforeend", '<meta name="theme-color" content="#0174DF"/><link rel="apple-touch-icon" href="https://exf.minecraft-hub.com/logos/icon-192.png"><link rel="manifest" href="/manifest.webmanifest">');
+document.head.insertAdjacentHTML("beforeend", '<meta name="theme-color" content="#0174DF"/><link rel="apple-touch-icon" href="https://exf.minecraft-hub.com/logos/icon-192.png"><link rel="manifest" href="/manifest.webmanifest">');
 
 window.addEventListener('beforeinstallprompt', (e) => {
   e.prompt();
